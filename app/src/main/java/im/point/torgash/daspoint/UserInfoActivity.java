f722e9bdf97eb58dcccf6db8b10d128d14774ca8
torgash-ivanblch <dissolver@megafonmail.ru>
2016-04-20 02:23:41 +0300

    userinfo now shows info and subscribe status remastered commentZone design, now EditText can be up to 96dp high

--- app/src/main/java/im/point/torgash/daspoint/UserInfoActivity.java
+++ app/src/main/java/im/point/torgash/daspoint/UserInfoActivity.java
@@ -1,5 +1,6 @@
 package im.point.torgash.daspoint;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,9 +10,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.util.Linkify;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.reginald.swiperefresh.CustomSwipeRefreshLayout;
@@ -20,6 +27,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Headers;
 import okhttp3.OkHttpClient;
@@ -31,9 +42,19 @@ public class UserInfoActivity extends AppCompatActivity {
     static final String AUTH_TOKEN = "auth_token";
     static final String CSRF_TOKEN = "csrf_token";
     static final int MSG_AVATAR = 1;
+    //let's define variables for our views
     NestedScrollView nestedScrollView;
     ImageView ivUserAvatar;
-    static boolean isInFront;
+    TextView userInfoAbout;
+    LinearLayout userInfoTextGroup;
+    ImageButton userInfoSubscribed;
+    ImageButton userInfoBlackListed;
+    ImageButton userInfoWhiteListed;
+    Toolbar toolbar;
+
+
+    LayoutInflater li;
+    static boolean isInFront; //just to ensure that the activity is not suspended
     protected SharedPreferences prefs;
     String token = "";
     String csrf_token = "";
@@ -56,7 +77,7 @@ public class UserInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         prefs = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
         setContentView(R.layout.activity_user_info);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         String title = getIntent().getStringExtra("login");
         if(null != title) setTitle(title);
@@ -64,6 +85,7 @@ public class UserInfoActivity extends AppCompatActivity {
         token = prefs.getString("token", "");
         csrf_token = prefs.getString("csrf_token", "");
 
+        li= (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,22 +94,99 @@ public class UserInfoActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //initializing view variables
         ivUserAvatar = (ImageView)findViewById(R.id.userInfoIvAvatar);
+        userInfoAbout = (TextView) findViewById(R.id.userInfoTvAbout);
+        userInfoTextGroup = (LinearLayout) findViewById(R.id.userInfoTextGroup);
+        userInfoSubscribed = (ImageButton) findViewById(R.id.userInfoBtnSubscribe);
+        userInfoBlackListed = (ImageButton) findViewById(R.id.userInfoBtnBlackList);
+        userInfoWhiteListed = (ImageButton) findViewById(R.id.userInfoBtnWhiteList);
         findUserInfo(title);
 
     }
     public void findUserInfo(final String login){
+        final int MSG_ERROR = 0;
         final Handler h = new Handler() {
             public void handleMessage(android.os.Message msg) {
                 switch (msg.what) {
                     case MSG_AVATAR:
                         if (UserInfoActivity.isInFront){
+                            JSONObject jsonUserInfo = (JSONObject) msg.obj;
+                            try {
+                                ImageLoader.getInstance().displayImage("http://i.point.im/a/280/" + jsonUserInfo.getString("avatar"), ivUserAvatar);
 
-                            ImageLoader.getInstance().displayImage("http://i.point.im/a/280/" + msg.obj.toString(), ivUserAvatar);
+                                if (jsonUserInfo.getBoolean("subscribed")) {
+
+                                    userInfoSubscribed.setBackgroundResource(R.drawable.circle_green);
+                                } else {
+                                    userInfoSubscribed.setBackgroundResource(R.drawable.circle_grey);
+                                }
+
+
+                                userInfoTextGroup.removeAllViews();
+                                if (!jsonUserInfo.getString("name").equals("")) {
+
+                                    userInfoTextGroup.addView(createTextView(jsonUserInfo.getString("name")));
+                                }
+                                if (!jsonUserInfo.getString("location").equals("")) {
+
+                                    userInfoTextGroup.addView(createTextView("Место: " + jsonUserInfo.getString("location")));
+                                }
+                                if (!jsonUserInfo.getString("about").equals("")) {
+
+                                    userInfoTextGroup.addView(createTextView("О себе:\n" + jsonUserInfo.getString("about")));
+                                }
+                                if (!jsonUserInfo.getString("homepage").equals("")) {
+
+                                    userInfoTextGroup.addView(createTextView("Сайт: " + jsonUserInfo.getString("homepage")));
+                                }
+                                if (!jsonUserInfo.getString("created").equals("")) {
+                                    String time = jsonUserInfo.get("created").toString();
+                                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+                                    Date qdate = new Date();
+                                    try {
+                                        qdate = sdf.parse(time);
+                                    } catch (ParseException e) {
+                                        Log.d("JSON", "Date parsing exception.", e);
+                                    }
+                                    SimpleDateFormat sdfout = new SimpleDateFormat("dd MMMM yyyy");
+                                    String created = "Зарегистрирован: " + sdfout.format(qdate);
+                                    userInfoTextGroup.addView(createTextView(created));
+                                }
+                                if (!jsonUserInfo.get("gender").toString().equals("null")) {
+                                    if(jsonUserInfo.getBoolean("gender")){
+
+                                        userInfoTextGroup.addView(createTextView("Пол: человек"));
+                                    }else{
+                                        userInfoTextGroup.addView(createTextView("Пол: женщина"));
+                                    }
+
+                                }else{
+                                    userInfoTextGroup.addView(createTextView("Пол: бот"));
+                                }
+                                if (!jsonUserInfo.getString("xmpp").equals("")) {
+
+                                    userInfoTextGroup.addView(createTextView("XMPP: " + jsonUserInfo.getString("xmpp")));
+                                }
+                                if (!jsonUserInfo.getString("email").equals("")) {
+
+                                    userInfoTextGroup.addView(createTextView("Email: " + jsonUserInfo.getString("email")));
+                                }
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
 //                            tvUserName.setText(prefs.getString("user_name", ""));
                         }
 
                         break;
+                    case MSG_ERROR:
+                        if(isInFront)Snackbar.make(toolbar, msg.obj.toString(), Snackbar.LENGTH_LONG)
+                                .setAction("Discard", null).show();
+                        userInfoAbout.setText("Загрузка не удалась");
+                        break;
+
 
                 }
             }
@@ -97,7 +196,9 @@ public class UserInfoActivity extends AppCompatActivity {
 
         Thread getUserInfo = new Thread(new Runnable() {
 
-            final OkHttpClient client = new OkHttpClient();
+            final OkHttpClient client = new OkHttpClient.Builder().readTimeout(30 * 1000, TimeUnit.MILLISECONDS)
+                    .connectTimeout(30 * 1000, TimeUnit.MILLISECONDS).writeTimeout(30 * 1000, TimeUnit.MILLISECONDS)
+                    .build();
 
             @Override
             public void run() {
@@ -119,23 +220,37 @@ public class UserInfoActivity extends AppCompatActivity {
                         Log.d("DP", responseHeaders.name(i) + ": " + responseHeaders.value(i));
                     }
 
-                    String userAvatar = jsonUserInfo.getString("avatar");
-                    String userName = jsonUserInfo.getString("name");
-                    //writing avatar link to prefs
 
-                    Message msg = h.obtainMessage(MSG_AVATAR, 0, 0, userAvatar);
+
+                    Message msg = h.obtainMessage(MSG_AVATAR, 0, 0, jsonUserInfo);
                     // отправляем
                     h.sendMessage(msg);
 
 
                 } catch (IOException e) {
                     e.printStackTrace();
+                    Message msg = h.obtainMessage(MSG_ERROR, "Network not available.");
+                    h.sendMessage(msg);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    Message msg = h.obtainMessage(MSG_ERROR, "Торгаш - хуй и не может в JSON");
+                    h.sendMessage(msg);
                 }
             }
         });
         getUserInfo.start();
+    }
+
+    //returns new View with a TextView inside to represent some info
+    private View createTextView(String text) {
+        View v = li.inflate(R.layout.text_view, null);
+        v.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        TextView tv = (TextView) v.findViewById(R.id.post_text_view);
+        tv.setText(text);
+        tv.setLinksClickable(true);
+        tv.setAutoLinkMask(Linkify.ALL);
+
+        return v;
     }
 }
